fcab9b62d57d056c4f9e3c7cd6fe5baf53bbbfde
Benjamin Crosnier <benjamin.crosnier@invenietis.com>
2015-05-22 10:42:02 +0200

    Added front-office views: Catalog and Album. Removed default ASP.NET elements from _Layout.

---
+++ Nwd.Web/Controllers/MusicController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Nwd.FrontOffice;
+using Nwd.FrontOffice.Impl;
+
+namespace Nwd.Web.Controllers
+{
+    public class MusicController : Controller
+    {
+        IMusicReader _musicReader;
+
+        public MusicController()
+        {
+            _musicReader = new MusicReader();
+        }
+
+        public ActionResult Catalog()
+        {
+            Nwd.FrontOffice.ViewModels.Catalog c = GetCatalog();
+
+            return View( c );
+        }
+
+        public ActionResult Album( int id )
+        {
+            var a = GetAlbumViewModel( id );
+
+            return View( a );
+        }
+
+        private AlbumViewModel GetAlbumViewModel( int albumId )
+        {
+            return _musicReader.GetAlbum( albumId );
+        }
+
+        private FrontOffice.ViewModels.Catalog GetCatalog()
+        {
+            return _musicReader.GetCatalog();
+        }
+    }
+}
